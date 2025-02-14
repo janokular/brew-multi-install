7e0cc81b496f90a378257c9f3e7c950caf6b3494
janekokularczyk@gmail.com <janekokularczyk@gmail.com>
2025-02-14 11:55:29 +0100

    staged changes

--- multi-install.sh
+++ multi-install.sh
@@ -2,40 +2,55 @@
 
 # This script installs Homebrew formulae and casks from a list
 
-PACKAGES='./packages'
-
 # Colors for messages
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
 
 usage() {
-  echo "Usage: ${0} [-f FILE]"
-  echo "Install all listed Homebrew formulae and casks: default ${PACKAGES}"
-  echo -e "-f FILE\tUse FILE for the list of Homebrew formulae and casks"
+  echo "Usage: ${0} [-c] FILE"
+  echo 'Install all listed Homebrew formulae and casks'
+  echo -e "-c\tTreat all named arguments as casks"
   exit 1
 }
 
 # Check options provided by the user
-while getopts f:v OPTION &> /dev/null
+while getopts c OPTION &> /dev/null
 do
   case ${OPTION} in
-    f) PACKAGES=${OPTARG} ;;
+    c) CASK='--cask' ;;
     ?) usage ;;
   esac
 done
 
+# Remove options while leaving the remaining arguments
+shift "$(( OPTIND - 1 ))"
+
+# Check if user provided only one file
+if [[ "${#}" -lt 1 ]]
+then
+  echo -e "${RED}Error:${NC} No file provided" >&2
+  usage
+elif [[ "${#}" -gt 1 ]]
+then
+  echo -e "${RED}Error:${NC} Too many files provided" >&2
+  usage
+fi
+
+# First argument is treated as PACKAGES
+PACKAGES="${1}"
+
 # Check if PACKAGES exists and is a file
 if [[ ! -f "${PACKAGES}" ]]
 then
-  echo -e "${RED}Cannot open file ${PACKAGES}${NC}" >&2
-  exit 1
+  echo -e "${RED}Error:${NC} Cannot open file ${PACKAGES}" >&2
+  usage
 fi
 
 # Check if PACKAGES is not empty
 if [[ ! -s "${PACKAGES}" ]]
 then
-  echo -e "${RED}Provided file ${PACKAGES} is empty${NC}" >&2
+  echo -e "${RED}Error:${NC} Provided file ${PACKAGES} is empty" >&2
   exit 1
 fi
 
@@ -52,11 +67,11 @@ fi
 while read -r PACKAGE
 do
   echo "Installing ${PACKAGE}"
-  brew install "${PACKAGE}"
+  brew install ${CASK} ${PACKAGE}
   echo
 done < "${PACKAGES}"
 
-# Remove outdated downloads for all formulae and casks
-brew cleanup &> /dev/null
+# Remove outdated downloads and caches for all formulae and casks
+brew cleanup --prune=all &> /dev/null
 
 exit 0
