717280701886fe4ed871236c900aef3092273f39
Jan <janekokularczyk@gmail.com>
2025-10-23 19:59:46 +0200

    refactored

--- multi-install.sh
+++ multi-install.sh
@@ -10,10 +10,9 @@ usage() {
 }
 
 # Check options provided by the user
-while getopts c OPTION &> /dev/null
-do
-  case ${OPTION} in
-    c) CASK='--cask' ;;
+while getopts c option &> /dev/null; do
+  case ${option} in
+    c) cask='--cask' ;;
     ?) usage ;;
   esac
 done
@@ -22,49 +21,42 @@ done
 shift "$(( OPTIND - 1 ))"
 
 # Check if user provided only one file
-if [[ "${#}" -lt 1 ]]
-then
+if [[ "${#}" -lt 1 ]]; then
   echo 'Error: No file provided' >&2
   usage
-elif [[ "${#}" -gt 1 ]]
-then
+elif [[ "${#}" -gt 1 ]]; then
   echo 'Error: Too many files provided' >&2
   usage
 fi
 
-# First argument is treated as PACKAGES
-PACKAGES="${1}"
+# First argument is treated as packages
+packages="${1}"
 
-# Check if PACKAGES exists and is a file
-if [[ ! -f "${PACKAGES}" ]]
-then
-  echo "Error: Cannot open file ${PACKAGES}" >&2
+# Check if packages exists and is a file
+if [[ ! -f "${packages}" ]]; then
+  echo "Error: Cannot open file ${packages}" >&2
   usage
 fi
 
-# Check if PACKAGES is not empty
-if [[ ! -s "${PACKAGES}" ]]
-then
-  echo "Error: Provided file ${PACKAGES} is empty" >&2
+# Check if packages is not empty
+if [[ ! -s "${packages}" ]]; then
+  echo "Error: Provided file ${packages} is empty" >&2
   exit 1
 fi
 
 # Check if Homebrew is installed
 brew -v &> /dev/null
-if [[ "${?}" -ne 0 ]]
-then
-  echo 'Warning: Homebrew is not installed on the system'
-  echo 'Installing Homebrew'
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [[ "${?}" -ne 0 ]]; then
+  echo 'Error: Homebrew is not installed on the system'
+  exit 1
 fi
 
-# Install all listed formulae and casks from the PACKAGES
-while read -r PACKAGE
-do
-  echo "Installing ${PACKAGE}"
-  brew install ${CASK} ${PACKAGE}
+# Install all listed formulae and casks from the packages
+while read -r package; do
+  echo "Installing ${package}"
+  brew install ${cask} ${package}
   echo
-done < "${PACKAGES}"
+done < "${packages}"
 
 # Remove outdated downloads and caches for all formulae and casks
 brew cleanup --prune=all &> /dev/null
